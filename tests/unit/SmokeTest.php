9a5ce5f2d3e4cc37bce8878c9b3159423c5f3b5b
Juanrendon17 <jaunrendoon@gmail.com>
2025-11-29 23:08:57 -0500

    123

---
+++ tests/unit/SmokeTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class SmokeTest extends TestCase
+{
+    public function testTrue(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testCanIncludeDb(): void
+    {
+        // Simple smoke test: verify db.php can be included
+        $this->assertFileExists('/var/www/html/db.php');
+    }
+
+    public function testCanIncludeGetTickets(): void
+    {
+        $this->assertFileExists('/var/www/html/get_tickets.php');
+    }
+
+    public function testCanIncludeAddTicket(): void
+    {
+        $this->assertFileExists('/var/www/html/add_ticket.php');
+    }
+}
